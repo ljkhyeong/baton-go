487bd8678ac5fedef8b6423b93151320710926c4
LJK <jolri24@gmail.com>
2026-09-05 18:03:19 +0900

    Fix: 정리된 링크의 새 멱등성 키 사용 안내 추가

--- application/src/main/java/com/personal/batongo/application/link/error/LinkPurgedException.java
+++ application/src/main/java/com/personal/batongo/application/link/error/LinkPurgedException.java
@@ -2,6 +2,6 @@ package com.personal.batongo.application.link.error;
 
 public class LinkPurgedException extends RuntimeException {
     public LinkPurgedException() {
-        super("보존 기간이 지나 정리된 링크입니다. 새 링크가 필요하면 새 생성 요청을 사용해 주세요");
+        super("보존 기간이 지나 정리된 링크입니다. 새 링크가 필요하면 새 Idempotency-Key로 생성해 주세요.");
     }
 }

acd6ba7a6e50816ab1a085be3d408937e034a36d
LJK <jolri24@gmail.com>
2026-09-05 17:53:59 +0900

    Fix: 기존 링크 URL 반환 실패 안내를 명확하게 수정

--- application/src/main/java/com/personal/batongo/application/link/error/LinkCodeReplayMismatchException.java
+++ application/src/main/java/com/personal/batongo/application/link/error/LinkCodeReplayMismatchException.java
@@ -3,6 +3,6 @@ package com.personal.batongo.application.link.error;
 public class LinkCodeReplayMismatchException extends RuntimeException {
 
     public LinkCodeReplayMismatchException() {
-        super("현재 링크 코드 파생 설정으로 기존 링크를 재생할 수 없습니다");
+        super("현재 코드 생성 설정으로 기존 URL을 반환할 수 없습니다");
     }
 }
